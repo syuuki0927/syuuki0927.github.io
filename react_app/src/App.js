1ff691833b59b3faabf0065a4108836a4128ff45
syuuki0927 <syuuki0927@gmail.com>
2022-01-01 04:54:04 +0900

    自己紹介を書いてみた

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,28 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Rect from './Rect';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render(){
+    return <div>
+      <h1 className="bg-primary text-white display-4">syuuki0927のページ</h1>
+      <div className="container">
+        <p className="subtitle">自己紹介</p>
+          <ul>
+            <li>群馬工業高等専門学校　電子情報工学科　卒業</li>
+            <li>豊橋技術科学大学　情報・知能工学課程　B3　在学中</li>
+            
+          </ul>
+        <p className="subtitle">することがないのだ</p>
+        <p>Github pagesなるものの存在を知り，積んでたreactの本を読んでなにかしようと思ったが特にやりたいことがなかった．
+          紹介するような自己がないのも問題．とりあえず本を読み終わるまで放置．
+          Under Construction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Rect x="200" y="500" w="200" h="200" c="#6992" r="25" />
+      </div>
     </div>
-  );
+  }
 }
 
 export default App;
